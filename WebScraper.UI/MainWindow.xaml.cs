fb346d6d2cc3060ec520d165ce7f13534bfb4a79
William Jelley <williamjelleydevman@gmail.com>
2022-03-14 08:13:34 +1300

    Creating barebones WPF application

---
+++ WebScraper.UI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebScraper.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HtmlParserTest_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: call out to business logic..?
+            Console.WriteLine("Button Clicked!");
+        }
+    }
+}
