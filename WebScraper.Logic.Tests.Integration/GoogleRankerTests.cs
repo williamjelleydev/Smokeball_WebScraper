ef464ec5bf61bebb9bf1bb2645e5b9153709ccd2
William Jelley <williamjelleydevman@gmail.com>
2022-03-14 17:15:40 +1300

    Refactor integration tests to rely only on IFixture DI

--- WebScraper.Logic.Tests.Integration/GoogleRankerTests.cs
+++ WebScraper.Logic.Tests.Integration/GoogleRankerTests.cs
@@ -1,13 +1,9 @@
-﻿using AutoFixture.Xunit2;
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
 using FluentAssertions;
-using Microsoft.Extensions.Logging;
 using Moq;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebScraper.Logic.HtmlParsers;
 using WebScraper.Logic.Tests.Integration.Customizations;
 using Xunit;
@@ -21,53 +17,51 @@ namespace WebScraper.Logic.Tests.Integration
             [Theory]
             [AutoMoqData]
             public void OneSmokeballSearchResultAtPosition5(
+                [Frozen] IFixture fixture,
                 [Frozen] IHtmlDownloader htmlDownloader,
-                ILogger<GoogleRanker> logger)
+                TagFactory tagFactory)
             {
-                // Hmm. How do I ensure that I actually get concrete implementations of everything here??
-                // For now, use concreate and manual ctro implementations of everything pls.
-                // then after functional, figure out how to magic autofixture to do what I want..
-
                 var expectedRanking = 5;
 
-                var tagFactory = new TagFactory();
-                var htmlParser = new HtmlParser(tagFactory);
+                fixture.Register<ITagFactory>(() => tagFactory);
+                fixture.Register<IHtmlParser>(() => fixture.Create<HtmlParser>());
 
                 // TODO: make relative file path so can run anywhere..
                 string htmlFilePath = @"C:\Source\WebScraper\WebScraper\WebScraper.Logic.Tests.Integration\TestData\GoogleTest.html";
                 var html = File.ReadAllText(htmlFilePath);
                 Mock.Get(htmlDownloader).Setup(x => x.DownloadHtml(It.IsAny<string>())).Returns(html);
 
-                var sut = new GoogleRanker(htmlDownloader, htmlParser, logger);
+                var sut = fixture.Create<GoogleRanker>();
 
-                var res = sut.GetRankings();
+                var result = sut.GetRankings();
 
-                res.Should().HaveCount(1);
-                res.First().Should().Be(expectedRanking);
+                result.Should().HaveCount(1);
+                result.First().Should().Be(expectedRanking);
             }
 
-            [Theory (Skip ="Enable once code actually meets this criteria lol")]
+            [Theory(Skip = "Enable once code actually meets this criteria lol")]
             [AutoMoqData]
             public void OneSmokeballSearchResultAtPosition7(
+                [Frozen] IFixture fixture,
                 [Frozen] IHtmlDownloader htmlDownloader,
-                ILogger<GoogleRanker> logger)
+                TagFactory tagFactory)
             {
                 var expectedRanking = 7;
 
-                var tagFactory = new TagFactory();
-                var htmlParser = new HtmlParser(tagFactory);
+                fixture.Register<ITagFactory>(() => tagFactory);
+                fixture.Register<IHtmlParser>(() => fixture.Create<HtmlParser>());
 
                 // TODO: make relative file path so can run anywhere..
                 string htmlFilePath = @"C:\Source\WebScraper\WebScraper\WebScraper.Logic.Tests.Integration\TestData\GoogleTest.html";
                 var html = File.ReadAllText(htmlFilePath);
                 Mock.Get(htmlDownloader).Setup(x => x.DownloadHtml(It.IsAny<string>())).Returns(html);
 
-                var sut = new GoogleRanker(htmlDownloader, htmlParser, logger);
+                var sut = fixture.Create<GoogleRanker>();
 
-                var res = sut.GetRankings();
+                var result = sut.GetRankings();
 
-                res.Should().HaveCount(1);
-                res.First().Should().Be(expectedRanking);
+                result.Should().HaveCount(1);
+                result.First().Should().Be(expectedRanking);
             }
 
         }
