b7cd64ea477b1e699206b1ae889908873c95d6dd
William Jelley <williamjelleydevman@gmail.com>
2022-03-15 02:25:54 +1300

    Show result on screen, looks average as lol

---
+++ WebScraper.UI/GoogleRankingViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebScraper.UI
+{
+    public class GoogleRankingViewModel : ViewModelBase
+    {
+
+        private string _rankingMessage;
+
+        public string RankingMessage
+        {
+            get { return _rankingMessage; }
+            set { SetProperty(ref _rankingMessage, value); }
+        }
+    }
+}
--- WebScraper.UI/MainWindow.xaml.cs
+++ WebScraper.UI/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WebScraper.Logic;
 
 namespace WebScraper.UI
@@ -23,59 +9,19 @@ namespace WebScraper.UI
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public class ViewModel : ViewModelBase
-    {
-
-        private string _rankingMessage;
-
-        public string RankingMessage
-        {
-            get { return _rankingMessage; }
-            set { SetProperty(ref _rankingMessage, value); }
-        }
-
-        // TODO: now _maybe_ from here, we click a button which then gets the result and sets rankingMessage
-        // In which case, this ViewModel will reflect the change in the UI!
-
-
-    }
-
-    public abstract class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = null)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                field = newValue;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-                return true;
-            }
-            return false;
-        }
-    }
-
-
     public partial class MainWindow : Window
     {
-        // TODO: make this its own GoogleRanker window or something??
         private readonly IGoogleRanker _googleRanker;
-
-        private readonly ViewModel _viewModel;
+        private readonly GoogleRankingViewModel _viewModel;
 
         public MainWindow(IGoogleRanker googleRanker)
         {
             _googleRanker = googleRanker;
 
-            _viewModel = new ViewModel();
-            _viewModel.RankingMessage = "Test Ranking Message";
+            _viewModel = new GoogleRankingViewModel();
             DataContext = _viewModel;
 
             InitializeComponent();
-
-            _viewModel.RankingMessage = "Mark";
-
         }
 
         // TODO: make this async
@@ -96,29 +42,23 @@ namespace WebScraper.UI
             var res = await _googleRanker.GetRankingsAsync();
         }
 
-        private async void GetAllRankingsTest_Click(object sender, RoutedEventArgs e)
-        {
-            //var res = await _googleRanker.GetAllRankingsAsync();
-        }
-
-        private void DownloadFreshHtml_Click(object sender, RoutedEventArgs e)
-        {
-            var url = "https://www.google.com.au/search?num=100&q=conveyancing+software";
-            var filePath = @"C:\Source\WebScraper\WebScraper\DownloadedFiles\FreshGoogleDownload.html";
-
-            string htmlDoc = "";
-            using (WebClient webClient = new WebClient())
-            {
-                // FUCK - how am I going to create an elgant serializer class lol?
-                webClient.DownloadFile(url, filePath);
-                //File.WriteAllText("Duh_test_html.txt", htmlDoc);
-                var stop = true;
-            }
-        }
+  
 
         private void ChangeRankingMessage_Click(object sender, RoutedEventArgs e)
         {
             _viewModel.RankingMessage = "La de da look at me!";
+        }
+
+        private async void GetRankings_Click(object sender, RoutedEventArgs e)
+        {
+            var rankings = await _googleRanker.GetRankingsAsync();
+            if (rankings.Any())
+            {
+                _viewModel.RankingMessage = $"Results found at positions: {string.Join(',', rankings)}";
+            } else
+            {
+                _viewModel.RankingMessage = "No results in the top 100";
+            }
         }
 
         // https://www.codeguru.com/dotnet/managing-non-blocking-calls-on-the-ui-thread-with-async-await/
